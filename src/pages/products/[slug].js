cc334f7ddf1d40a7d87f5ba030bd2f692dae67ca
Helen Langridge <langridge.helen@outlook.com>
2023-02-26 19:02:08 +0000

    add SEO meta information

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,12 +1,21 @@
+import Head from 'next/head'
 import { fromImageToUrl } from 'utils/urls'
 import products from '../../products.json'
 const product = products.data[0]
 
 const Product = () => {
-    console.log(products)
+    // console.log(products)
 
     return (
         <div>
+            <Head>
+                {product.attributes.meta_title &&
+                    <title>{product.attributes.meta_title}</title>
+                }
+                {product.attributes.meta_description &&
+                    <meta name="description" content={product.attributes.meta_description}/>
+                }
+            </Head>
             <h3>{product.attributes.name}</h3>
             <img src={fromImageToUrl(product.attributes.image)}/>
             <h3>{product.attributes.name}</h3>
